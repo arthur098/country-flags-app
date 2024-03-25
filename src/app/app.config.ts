fc864be8239973c2de85eb0268971eb704824aa3
Arthur <arthur.rodrigues@protonmail.com>
2024-03-24 23:42:47 -0300

    Feature
    Consuming api for flag listing;
    Applying filter for flags listing;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule, BrowserAnimationsModule)]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatCard, MatCardContent, MatCardImage } from '@angular/material/card';
 import { FlagService } from '../../service/flag.service';
 import { FlagInfoModel } from '../../model/FlagInfoModel';
-import { NgFor } from '@angular/common';
+import { NgForOf } from '@angular/common';
+import { MatFormField, MatLabel, MatPrefix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'cf-home',
@@ -11,7 +16,15 @@ import { NgFor } from '@angular/common';
     MatCard,
     MatCardImage,
     MatCardContent,
-    NgFor
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIcon,
+    MatPrefix,
+    NgForOf,
+    MatSelect,
+    MatOption,
+    ReactiveFormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -21,10 +34,28 @@ export class HomeComponent implements OnInit {
   constructor(public flagService: FlagService) {}
 
   flags: FlagInfoModel[] = [];
+  flagList: FlagInfoModel[] = [];
+  regions: Set<string> = new Set();
+  form: FormGroup = new FormGroup<any>({
+    country: new FormControl(''),
+    region: new FormControl('')
+  });
 
   ngOnInit(): void {
     this.flagService.findFlagsInformations().subscribe((result: FlagInfoModel[]) => {
       this.flags = result;
+      this.flagList = result;
+
+      let regions = this.flags.map(flag => flag.region);
+      regions = regions.sort();
+      this.regions = new Set(regions);
     });
+  }
+
+  filter():void {
+    const country = this.form.value.country;
+    const region = this.form.value.region;
+
+    this.flags = this.flagList.filter(flag => flag.name.toUpperCase().includes(country.toUpperCase()) && (flag.region === region || !region));
   }
 }
