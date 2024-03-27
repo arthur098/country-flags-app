73f53da31bfdcf3f8c627e0c8be9f557ca942552
Arthur <arthur.rodrigues@protonmail.com>
2024-03-26 23:06:55 -0300

    Feature
    Initial dark mode implementation;
    Finishing mobile layout implementation;

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { MatIcon } from "@angular/material/icon";
-import {MatButton} from "@angular/material/button";
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'cf-header',
@@ -10,5 +10,14 @@ import {MatButton} from "@angular/material/button";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  toggleDarkMode() {
+    const themeClass = 'dark-theme';
+    const body = document.body;
 
+    if(body.classList.contains(themeClass)) {
+      body.classList.remove(themeClass);
+    } else {
+      body.classList.add(themeClass);
+    }
+  }
 }
